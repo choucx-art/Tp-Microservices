f72e655a0df354745c5deeff4a4f50e01590a9be
choucx-art <66334386+choucx-art@users.noreply.github.com>
2025-05-05 12:32:00 +0100

    Update index.js

--- service-product/index.js
+++ service-product/index.js
@@ -29,3 +29,5 @@ app.get('/metrics', async (req, res) => {
   res.end(await client.register.metrics());
 });
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongodb-user:27017/users');
