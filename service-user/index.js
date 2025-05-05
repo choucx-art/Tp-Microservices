f2bce6602ab5edb3050733173dca9c6f8007ff1f
choucx-art <66334386+choucx-art@users.noreply.github.com>
2025-05-05 12:19:04 +0100

    Update index.js

--- service-user/index.js
+++ service-user/index.js
@@ -23,3 +23,12 @@ app.listen(PORT, async () => {
   console.log(`Service User sur le port ${PORT}`);
   await connect();
 });
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
