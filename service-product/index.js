848f154dc51e2a835b8ff3ca6305e6f52bb6f3df
choucx-art <66334386+choucx-art@users.noreply.github.com>
2025-05-05 12:19:34 +0100

    Update index.js

--- service-product/index.js
+++ service-product/index.js
@@ -20,3 +20,12 @@ app.listen(PORT, async () => {
   await connect();
 });
 
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
