eb44d9da64fa65a10d695b600bc725e4e44ebb8d
choucx-art <66334386+choucx-art@users.noreply.github.com>
2025-05-13 08:31:10 +0100

    Update paymentRoutes.js

--- payment-service/src/routes/paymentRoutes.js
+++ payment-service/src/routes/paymentRoutes.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const controller = require('../controllers/paymentController');
 const router = express.Router();
+const paymentController = require('../controllers/paymentController');
 
-const authenticate = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.sendStatus(401);
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
-router.post('/', authenticate, controller.processPayment);
-router.get('/', authenticate, controller.getUserPayments);
+router.post('/', paymentController.createPayment);
+router.get('/', paymentController.getAllPayments);
 
 module.exports = router;
