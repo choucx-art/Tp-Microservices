7695230c97c776e1155aed7a14be8c67f736602f
choucx-art <66334386+choucx-art@users.noreply.github.com>
2025-05-13 08:19:50 +0100

    Update userRoutes.js

--- user-service/src/routes/userRoutes.js
+++ user-service/src/routes/userRoutes.js
@@ -1,21 +1,9 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const controller = require('../controllers/Usercontroller');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-// Middleware JWT pour la route protégée
-const authenticate = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.sendStatus(401);
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
-router.post('/register', controller.register);
-router.post('/login', controller.login);
-router.get('/profile', authenticate, controller.profile);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
